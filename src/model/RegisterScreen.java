88fbc94bc91afbf5570129af7197ed0fb23c2f46
Estiven Reyes <estivenreyes2013@hotmai.com>
2020-12-07 01:49:13 -0500

    RegisterScreen methods

--- src/model/RegisterScreen.java
+++ src/model/RegisterScreen.java
@@ -2,8 +2,10 @@ package model;
 
 import java.util.ArrayList;
 
+import processing.core.PApplet;
+
 public class RegisterScreen {
-	//private ArrayList<Player> player
+	private ArrayList<Player> player;
 	private int currentPlayer;
 	private static RegisterScreen onlyIntence;
 	
@@ -14,5 +16,57 @@ public class RegisterScreen {
 		return onlyIntence;
 		
 	}
+	
+	public void sort() {}
+	
+	public void initialPlayer(PApplet app) {
+		Player p= new Player("Mejia007",null, null, null, app);
+		player.add(p);
+	}
+	
+	public void addPlayer(String name, String time, String score, String date, PApplet app) {
+		Player p = new Player(name, time, score, date, app);
+		player.add(p);
+		
+		for (int i = 0; i < player.size(); i++) {
+			System.out.println(player.get(i).getName());
+			System.out.println(player.get(i).getTime());
+			System.out.println(player.get(i).getScore());
+			System.out.println(player.get(i).getDate());
+			System.out.println("works");
+		}
+	}
+	
+	public int[] searchPlayer(String name) {
+		boolean found = false;
+		int[] info = new int [1];
+		info[0] =0;
+		info[1] = 1;
+		for (int i = 0; i < player.size() && found == false; i++) {
+			String compareName = player.get(i).getName();
+			if(name.equals(compareName)) {
+				found = true;
+				info[0] =1;
+				info[1] =i;
+			}
+		}
+		return info;
+	}
+	
 
+	public ArrayList<Player> getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(ArrayList<Player> player) {
+		this.player = player;
+	}
+
+	public int getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+	public void setCurrentPlayer(int currentPlayer) {
+		this.currentPlayer = currentPlayer;
+	}
 }
